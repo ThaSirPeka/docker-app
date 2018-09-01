970d4e238bf741f7cbf8eeb947bd7d30e71e874b
Javier Gómez <javier.gomez@adsmurai.com>
2018-09-02 00:46:21 +0200

    Add first test entity and migration
    A new php-cli image with mysql is needed to set the driver

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class DashboardController
+class DashboardController extends AbstractController
 {
     public function index()
     {
+        /** @var Product $product */
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find(1);
         return new Response(
-            '<html><body>Bienvenido</body></html>'
+            '<html><body>Bienvenido. Hay 1 producto: '. $product->getName().'</body></html>'
         );
     }
 }
